b466bf77631481eb6bd3c2870e0caddb96414bee
j.mokhtari <j.mokhtari@matiran.ir>
2026-06-09 16:31:28 +0330

    Write better intersection, union and iou functions, added errors to get_path_from_cstr function

--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,38 @@
 use crate::models::FaceBox;
-use std::ffi::CStr;
+use crate::errors::FaceDetectionResponse;
+use std::ffi::{c_int, CStr};
 use std::os::raw::c_char;
+use std::path::Path;
 
-pub fn intersection(box1: &FaceBox, box2: &FaceBox) -> f32 {
-    (box1.x2.min(box2.x2) - box1.x1.max(box2.x1)) * (box1.y2.min(box2.y2) - box1.y1.max(box2.y1))
+pub fn iou(a: &FaceBox, b: &FaceBox) -> f32 {
+    let inter = intersection(a, b);
+    let union = union(a, b);
+    if union == 0.0 { 0.0 } else { inter / union }
 }
 
-pub fn union(box1: &FaceBox, box2: &FaceBox) -> f32 {
-    ((box1.x2 - box1.x1) * (box1.y2 - box1.y1)) + ((box2.x2 - box2.x1) * (box2.y2 - box2.y1))
-        - intersection(box1, box2)
+fn intersection(a: &FaceBox, b: &FaceBox) -> f32 {
+    let x1 = a.x1.max(b.x1);
+    let y1 = a.y1.max(b.y1);
+    let x2 = a.x2.min(b.x2);
+    let y2 = a.y2.min(b.y2);
+    if x1 >= x2 || y1 >= y2 { 0.0 } else { (x2 - x1) * (y2 - y1) }
 }
 
-pub fn get_path_from_cstr<'a>(path_ptr: *const c_char) -> Result<&'a str, i32> {
+fn union(a: &FaceBox, b: &FaceBox) -> f32 {
+    let area_a = (a.x2 - a.x1) * (a.y2 - a.y1);
+    let area_b = (b.x2 - b.x1) * (b.y2 - b.y1);
+    area_a + area_b - intersection(a, b)
+}
+
+pub fn get_path_from_cstr<'a>(path_ptr: *const c_char) -> Result<&'a Path, c_int> {
     if path_ptr.is_null() {
-        return Err(-1);
+        return Err(FaceDetectionResponse::NullPointerError as c_int);
     }
 
-    unsafe { CStr::from_ptr(path_ptr) }
+    let path_str = unsafe { CStr::from_ptr(path_ptr) }
         .to_str()
-        .map_err(|_| -2)
+        .map_err(|_| FaceDetectionResponse::PathUTF8Error as c_int)?;
+
+    let path = Path::new(path_str);
+    Ok(path)
 }
